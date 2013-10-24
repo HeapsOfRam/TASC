62d66df3db055343fe0cd0179d89acaba1f74aa9
Ryan March <rmarch03@villanova.edu>
2013-10-23 21:58:41 -0400

    Refactoring after initial pages

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe "Pages" do
 
+	let(:base_title) { "TASC" }
+
 	describe "Home page" do
 		it "should have the content 'TASC'" do
 			visit '/pages/home'
 			expect(page).to have_content('TASC')
+		end
+
+		it "should have the title 'Home'" do
+			visit '/pages/home'
+			expect(page).to have_title("#{base_title} | Home")
 		end
 	end
 
@@ -14,12 +21,22 @@ describe "Pages" do
 			visit '/pages/help'
 			expect(page).to have_content('Help')
 		end
+
+		it "should have the title 'Help'" do
+			visit '/pages/help'
+			expect(page).to have_title("#{base_title} | Help")
+		end
 	end
 
 	describe "About page" do
 		it "should have the content 'About'" do
 			visit '/pages/about'
 			expect(page).to have_content('About')
+		end
+
+		it "should have the title 'About'" do
+			visit '/pages/about'
+			expect(page).to have_title("#{base_title} | About")
 		end
 	end
 
@@ -28,12 +45,22 @@ describe "Pages" do
 			visit '/pages/members'
 			expect(page).to have_content('Members')
 		end
+
+		it "should have the title 'Members'" do
+			visit '/pages/members'
+			expect(page).to have_title("#{base_title} | Members")
+		end
 	end
 
 	describe "Sites page" do
 		it "should have the content 'Sites'" do
 			visit '/pages/sites'
 			expect(page).to have_content('Site')
+		end
+
+		it "should have the title 'Sites'" do
+			visit '/pages/sites'
+			expect(page).to have_title("#{base_title} | Sites")
 		end
 	end
 	
